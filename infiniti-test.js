c70091caac13ce3e8c37fd1bb6fa242e7d2feb28
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-15 19:28:20 +0800

    Updated server/client test.

--- infiniti-test.js
+++ infiniti-test.js
@@ -3,19 +3,19 @@ const Infiniti = require( "./infiniti.js" );
 
 let sample1 = new Date( "8/15/2016" );
 
-assert.equal( Infiniti( sample1 ).printDate( ), "August 15, 2016" );
-assert.equal( Infiniti( sample1 ).getDate( ), "August 15, 2016" );
-assert.equal( Infiniti( sample1 ).realDate( ), "2016-08-14T16:00:00" );
-assert.equal( Infiniti( sample1 ).relativeDate( ), "2016-08-15T00:00:00" );
+assert.equal( Infiniti( sample1 ).printDate( ), "August 15, 2016", "should be equal" );
+assert.equal( Infiniti( sample1 ).getDate( ), "August 15, 2016", "should be equal" );
+assert.equal( Infiniti( sample1 ).realDate( ), "2016-08-14T16:00:00", "should be equal" );
+assert.equal( Infiniti( sample1 ).relativeDate( ), "2016-08-15T00:00:00", "should be equal" );
 
 let Infiniti_date = Infiniti( sample1 ).trueDate;
-assert.equal( Infiniti_date, "0​2016​08​14​16​00​00​00480" );
+assert.equal( Infiniti_date, "0​2016​08​14​16​00​00​00480", "should be equal" );
 
 let compact1 = Infiniti( sample1 ).compact( );
-assert.deepEqual( compact1, [ 2016081416, 480 ] );
+assert.deepEqual( compact1, [ 2016081416, 480 ], "should be deeply equal" );
 
 let comparison1 = Infiniti( Infiniti_date ).parse( );
 let comparison2 = Infiniti( compact1 ).parse( );
-assert.equal( comparison1.trueDate, comparison2.trueDate );
+assert.equal( comparison1.trueDate, comparison2.trueDate, "should be equal" );
 
 console.log( "ok" );
